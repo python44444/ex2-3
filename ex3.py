b3baf674cb8fc86a76b0e0ba97ec32b5480b18a6
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-11-14 16:05:11 +0900

    update

--- ex3.py
+++ ex3.py
@@ -19,27 +19,27 @@ def find_eye(original_face_image):
 
 
 def paste_img(eye_list):
-    r_x = eye_list[2][0]  # X座標
-    r_y = eye_list[2][1]  # Y座標
-    r_w = eye_list[2][2]  # 横幅
-    r_h = eye_list[2][3]  # 縦幅
-    print(r_x, r_y, r_w, r_h)
-
-    l_x = eye_list[1][0]  # X座標
-    l_y = eye_list[1][1]  # Y座標
-    l_w = eye_list[1][2]  # 横幅
-    l_h = eye_list[1][3]  # 縦幅
-    print(l_x, l_y, l_w, l_h)
-
     face_img = Image.open(original_face_image)
     eye_img = Image.open(original_eye_image)
 
-    new_r_eye_img = eye_img.resize((r_w, r_h))
-    new_l_eye_img = eye_img.resize((l_w, l_h))
-    new_r_eye_img.save("images/resized_r_eye_img.png")
-    new_l_eye_img.save("images/resized_l_eye_img.png")
+    # right_eye
+    r_x = eye_list[2][0]
+    r_y = eye_list[2][1]
+    r_w = eye_list[2][2]
+    r_h = eye_list[2][3]
 
+    new_r_eye_img = eye_img.resize((r_w, r_h))
+    new_r_eye_img.save("images/resized_r_eye_img.png")
     face_img.paste(new_r_eye_img, (r_x, r_y), new_r_eye_img.split()[3])
+
+    # left_eye
+    l_x = eye_list[1][0]
+    l_y = eye_list[1][1]
+    l_w = eye_list[1][2]
+    l_h = eye_list[1][3]
+    
+    new_l_eye_img = eye_img.resize((l_w, l_h))
+    new_l_eye_img.save("images/resized_l_eye_img.png")
     face_img.paste(new_l_eye_img, (l_x, l_y), new_l_eye_img.split()[3])
 
     face_img.save("images/pasted_face_img.png")
